b19bb71742ea1e34739ed35d68873f2a4078908d
j-parthiv <parthivjani9@gmail.com>
2023-08-02 00:56:54 +0200

    Add all filter's functionality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -58,9 +58,49 @@ const filter_reducer = (state, action) => {
     return {...state, filters:{...state.filters, [name]: value}}
   }
   if(action.type === FILTER_PRODUCTS){
-    return {...state}
-  }
-  if(action.type === CLEAR_FILTERS){
+    const {all_products} = state
+    const {text, category, company, color, price, shipping} = state.filters
+
+    let tempProducts = [...all_products]
+    // text
+    if(text){
+      tempProducts = tempProducts.filter((product) =>{
+        return product.name.toLowerCase().startsWith(text)
+      })
+    }
+    // category
+    if(category !== 'all'){
+      tempProducts = tempProducts.filter((product)=>{
+        return product.category === category
+      })
+    }
+    // company
+    if(company !== 'all'){
+      tempProducts = tempProducts.filter((product)=>{
+        return product.company === company
+      })
+    }
+    // colors
+    if(color !== 'all'){
+      tempProducts = tempProducts.filter((product)=>{
+        return product.colors.find((c) => c === color)
+      })
+    }
+    // price
+     tempProducts = tempProducts.filter((product)=>{
+        return product.price <= price
+      })
+    
+    // shipping
+    if(shipping){
+      tempProducts = tempProducts.filter((product)=>{
+        return product.shipping === true
+      })
+    }
+    return {...state, filtered_products: tempProducts}
+    }
+ 
+  if (action.type === CLEAR_FILTERS) {
     return {
       ...state,
       filters: {
@@ -71,10 +111,9 @@ const filter_reducer = (state, action) => {
         color: 'all',
         price: state.filters.max_price,
         shipping: false,
-      }
+      },
     }
   }
-
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 

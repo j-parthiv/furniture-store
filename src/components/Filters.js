eccf7e8c1087bb88fddb4c7d423e49dbb9fa4188
j-parthiv <parthivjani9@gmail.com>
2023-08-01 14:19:19 +0200

    Add Companies Filter Option

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -60,6 +60,14 @@ const Filters = () => {
               })}
             </div>
           </div>
+          <div className="form-control">
+              <h5>company</h5>
+              <select name="company" value={company} onChange={updateFilters} className="company">
+                {companies.map((company, index) =>{
+                  return <option key={index} value={company}>{company}</option>
+                })}
+              </select>
+          </div>
         </form>
       </div>
     </Wrapper>

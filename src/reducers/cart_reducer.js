8ff40f24d4787603d640717a4c4dd8a6318a85fa
j-parthiv <parthivjani9@gmail.com>
2023-08-02 01:42:33 +0200

    Handle Existing Items amount in AddToCart Reducer

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -5,13 +5,24 @@ import {
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
 } from '../actions'
+import CartItem from '../components/CartItem';
 
 const cart_reducer = (state, action) => {
   if(action.type === ADD_TO_CART){
     const {id, color, amount, product} = action.payload;
     const tempItem = state.cart.find((i)=> i.id === id + color)
     if(tempItem){
-
+      const tempCart = state.cart.map((CartItem) =>{
+        if(CartItem.id === id + color){
+          let newAmount = cartItem.amount + amount
+          if(newAmount > cartItem.max){{
+            newAmount = cartItem.max
+          }}else{
+            return cartItem
+          }
+        }
+      })
+      return {...state, cart: tempCart}
     }else{
       const newItem = {
         id: id + color,
@@ -21,7 +32,6 @@ const cart_reducer = (state, action) => {
         image: product.images[0].url,
         price: product.price,
         max: product.stock
-
       }
       return {...state, cart:[...state.cart,newItem ]}
     }

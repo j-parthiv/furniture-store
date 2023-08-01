7ec180380ca7d4491faa206d73bd1f31a8a919ec
j-parthiv <parthivjani9@gmail.com>
2023-08-02 01:33:20 +0200

    Set addToCart Reducer to handle new item

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -7,6 +7,25 @@ import {
 } from '../actions'
 
 const cart_reducer = (state, action) => {
+  if(action.type === ADD_TO_CART){
+    const {id, color, amount, product} = action.payload;
+    const tempItem = state.cart.find((i)=> i.id === id + color)
+    if(tempItem){
+
+    }else{
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount, 
+        image: product.images[0].url,
+        price: product.price,
+        max: product.stock
+
+      }
+      return {...state, cart:[...state.cart,newItem ]}
+    }
+  }
   return state
   throw new Error(`No Matching "${action.type}" - action type`)
 }

b380cedf68aa6c54fee2c6781e6df3b717309168
j-parthiv <parthivjani9@gmail.com>
2024-01-21 00:28:21 +0100

    refactor: change code alignment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,20 @@
-import Home from './HomePage'
-import Products from './ProductsPage'
-import SingleProduct from './SingleProductPage'
-import About from './AboutPage'
-import Cart from './CartPage'
-import Error from './ErrorPage'
-import Checkout from './CheckoutPage'
-import PrivateRoute from './PrivateRoute'
-
-export {Home, Products, SingleProduct, About, Cart, Error, Checkout, PrivateRoute}
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+import AuthWrapper from "./AuthWrapper" ;
+export {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+  AuthWrapper,
+};

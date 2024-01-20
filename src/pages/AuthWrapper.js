aad71e51c2d2a854eac2f7377cf8d3b0f4d8a2c6
j-parthiv <parthivjani9@gmail.com>
2024-01-21 00:19:03 +0100

    feat: make an auth wrapper component

--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -2,8 +2,21 @@ import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import styled from 'styled-components'
 
-const AuthWrapper = () => {
-  return <h4>AuthWrapper Component</h4>
+const AuthWrapper = ({children}) => {
+  const {isLoading, error} = useAuth0()
+  if(isLoading){
+    return <Wrapper>
+      <h1>Loading...</h1>
+    </Wrapper>
+  }
+  if(error){
+    return <Wrapper>
+      <h1>{error.message}</h1>
+    </Wrapper>
+  }
+  return <>
+  {children}
+  </>
 }
 
 const Wrapper = styled.section`
@@ -12,4 +25,4 @@ const Wrapper = styled.section`
   place-items: center;
 `
 
-export default AuthWrapper
+export default AuthWrapper;
